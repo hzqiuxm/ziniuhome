2bc74234a4ad1038e62fcffff7ecb9b494cd2429
youm <youming@yunhetong.net>
2017-09-26 16:17:17 +0800

    重新整理了项目框架，把所有没用的都干掉了，只留了一个干净的框架，后期需要集成其他功能，各凭本事自己动手吧！

--- model/src/main/resources/com.ziuniu.mapper/sql/UmSecUserRole.sql
+++
@@ -1,9 +0,0 @@
--- auto Generated on 2017-05-07 17:16:09 
--- DROP TABLE IF EXISTS `um_sec_user_role`; 
-CREATE TABLE `um_sec_user_role`(
-    `id` INT (11) UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT COMMENT 'id',
-    `user_id` INT (11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'userId',
-    `role_id` INT (11) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'roleId',
-    `gmt_create` DATETIME NOT NULL DEFAULT current_timestamp COMMENT 'gmtCreate',
-    PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '`um_sec_user_role`';

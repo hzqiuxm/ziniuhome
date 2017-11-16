7cfc3497241665b0721dd5bfcae681fa4813e878
xuyizhong@yunhetong.net <xuyizhong@yunhetong.net>
2017-11-16 10:58:10 +0800

    配置文件修改 markDown语法适配

--- webapp/src/main/java/com/ziniu/config/SecurityConfig.java
+++ webapp/src/main/java/com/ziniu/config/SecurityConfig.java
@@ -15,6 +15,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+/**
+ * Copyright © 2017年 ziniuxiaozhu. All rights reserved.
+ *
+ * @author shengwuyou
+ * @data 2017/11/16 0015 10：55
+ */
+
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true) //http://www.mamicode.com/info-detail-1150834.html
